9edae3438db86484d7f133cc3f1dca086f683661
gabe r <somemonewithalife@gmail.com>
2022-11-01 19:29:10 -0400

    added some js

---
+++ index.js
@@ -0,0 +1,11 @@
+const rows = 5
+const columns = 6
+let output
+
+for (let i=0; i < rows; i+=1){
+    for (let j=0; j < columns; j+=1){
+        output+='*'
+    }
+    console.log(output)
+    output=''
+}
